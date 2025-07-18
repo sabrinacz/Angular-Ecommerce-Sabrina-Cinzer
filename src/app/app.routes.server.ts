48d4bdcbc67b8a73f92138a9365fefee9a73c3da
Sabrina Cinzer <sabrinacinzer@gmail.com>
2025-07-18 02:10:17 -0300

    updated prerender

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,7 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 import { routes } from './app.routes';
 import { routesIDs } from '../shared/mocks/routes-ids';
+import { PostsService } from './services/posts.service';
 
 
 export const serverRoutes: ServerRoute[] = [
@@ -8,8 +9,9 @@ export const serverRoutes: ServerRoute[] = [
     path: 'products/:product_id',
     renderMode: RenderMode.Prerender,
    async getPrerenderParams() {
-      const ids = routesIDs;
-      return ids.map(id => ({ id }));
+    const dataService = inject(PostsService);
+    const ids = await dataService.getProductsIds();
+    return ids.map(id => ({ product_id: id }));
   }},
   {
     path: '**',
