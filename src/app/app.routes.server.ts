706f6abcfadcd5fd592cf4b5b9e659d3e5169e92
Sabrina Cinzer <sabrinacinzer@gmail.com>
2025-07-18 12:13:38 -0300

    Update app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,19 +1,15 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+import { RenderMode, ServerRoute  } from '@angular/ssr';
 import { inject } from '@angular/core';
 import { ProductsService } from './services/products.service';
+
 
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Server,
   },
   {
     path: 'products/:product_id',
     renderMode: RenderMode.Server,
-    async getPrerenderParams() {
-      const productsService = inject(ProductsService);
-      const products = await firstValueFrom(productsService.getProducts());
-      return products.map(product => ({ product_id: product.id.toString() }));
-    }
   }
 ];
