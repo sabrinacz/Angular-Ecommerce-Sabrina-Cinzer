e1474459bed8fbfde956074f99c199880e8016b5
Sabrina Cinzer <sabrinacinzer@gmail.com>
2025-07-18 12:02:55 -0300

    home

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Products } from '../products/products';
+import { ProductsService, ProductInterface } from '../services/products.service';
+
 
 @Component({
   selector: 'app-home',
@@ -7,10 +9,22 @@ import { Products } from '../products/products';
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
+
 export class Home {
-  constructor() {
+  products: ProductInterface[] = [];
+  loading: boolean = true;
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit() {
+    this.productsService.getProducts().subscribe({
+      next: (products) => {
+        this.products = products;   
+        this.loading = false;      
+      },
+      error: (error) => {
+        console.error('Error obteniendo productos', error);
+        this.loading = false;        
+      }
+    });
   }
-  products = [];
-  loading = true;
-  
-  }
+}
