96309fa28afe4eed2918c72224ec10d7520c3da7
Sabrina Cinzer <sabrinacinzer@gmail.com>
2025-07-18 11:42:59 -0300

    routes.server

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,15 +1,9 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
-import { routes } from './app.routes';
-import { routesIDs } from '../shared/mocks/routes-ids';
 
+import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'products/:product_id',
-    renderMode: RenderMode.Server,
-  },
-  {
     path: '**',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Prerender
   }
 ];
